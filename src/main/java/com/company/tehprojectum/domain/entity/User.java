fdfaa956c8f785fbb3687fba230173f786a501e9
Ygarr <Urbanterror1>
2017-03-10 15:46:31 +0200

    postgresql etc

--- src/main/java/com/company/tehprojectum/domain/entity/User.java
+++ src/main/java/com/company/tehprojectum/domain/entity/User.java
@@ -34,8 +34,10 @@ public class User extends DomainBase {
 
   @Id
 	@Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
-	@SequenceGenerator(name = "users_seq", sequenceName = "users_seq", allocationSize = 1)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+//for h2db:
+//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
+//	@SequenceGenerator(name = "users_seq", sequenceName = "users_seq", allocationSize = 1)
   public Long getId() {
     return this.id;
   }
